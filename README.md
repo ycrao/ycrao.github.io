68dbea9758e4dc694025778f432fcbb72b3ed8cd
raoyc <raoyc2009@gmail.com>
2016-07-18 15:58:23 +0800

    first update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# ycrao.github.io
+# 个人ID展示页面
+
 http://raoyc.com
+
+只是几个静态页面。
---
+++ js/script.js
@@ -0,0 +1,9 @@
+/*
+ * @title       script
+ * @name        script.js
+ * @author      raoyc
+ * @link        http://raoyc.com/
+ * @copyright   Copyright (c) 2014
+ */
+
+/*write some javascript code*/
