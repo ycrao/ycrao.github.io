29ec6c38684d7672e600127611034e4f567ef5e0
raoyc <raoyc2009@gmail.com>
2016-08-10 00:44:05 +0800

    using online assets

--- js/script.js
+++
@@ -1,9 +0,0 @@
-/*
- * @title       script
- * @name        script.js
- * @author      raoyc
- * @link        http://raoyc.com/
- * @copyright   Copyright (c) 2014
- */
-
-/*write some javascript code*/
